ca95dba55d3c3973886ef59e820740d5528e9d62
Kevin Earls <kevinearls@mac.com>
2024-04-11 19:52:00 +0200

    Initial Commit

---
+++ SplitsVille/SplitsVilleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitsVilleApp.swift
+//  SplitsVille
+//
+//  Created by Kevin Earls on 11/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct SplitsVilleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
