8d81fb2ad5cb6fb25619105e0e4d3a0a7a6e6742
Kevin Earls <kevinearls@mac.com>
2024-04-14 17:41:14 +0200

    More silly splash screen stuff - time to move on
    Signed-off-by: Kevin Earls <kevinearls@mac.com>

--- SplitsVille/Views/SplashScreenView.swift
+++ SplitsVille/Views/SplashScreenView.swift
@@ -5,7 +5,6 @@
 //  Created by Kevin Earls on 13/04/2024.
 //
 // Based on: https://www.youtube.com/watch?v=lBCpwYDljwI&t=7s
-
 import SwiftUI
 
 struct SplashScreenView: View {
@@ -15,20 +14,29 @@ struct SplashScreenView: View {
   @State private var scale = CGSize(width: 0.8, height: 0.8)
   @State private var zStackOpacity = 1.0
   @State var isDollarSignSpinning = true
+  let currencySign = ["dollarsign", "yensign", "eurosign", "sterlingsign"].randomElement()
 
   var body: some View {
     ZStack {
       Color.green.ignoresSafeArea()
       ZStack {
-        Image(systemName: "dollarsign")    // TODO pick one randomly from a known set?
-          .foregroundStyle(.white)
-          .font(.system(size: systemImageSize))   // TODO create a constant
-          .opacity(systemImageOpacity)
-          .rotationEffect(.degrees(isDollarSignSpinning ? 360 : 0))
+        VStack {
+          if let currencySign {
+            Image(systemName: currencySign)
+              .foregroundStyle(.white)
+              .font(.system(size: systemImageSize))
+              .opacity(systemImageOpacity)
+              .rotationEffect(.degrees(isDollarSignSpinning ? 360 : 0))
+          }
+          Text("$plitsvill€")
+            .foregroundStyle(.white)
+            .font(.system(size: systemImageSize * 0.6))
+            .opacity(systemImageOpacity)
+        }
       }
       .scaleEffect(scale)
       .onAppear {
-        withAnimation(.easeIn(duration: 3)) {
+        withAnimation(.easeIn(duration: 2)) {
           isDollarSignSpinning.toggle()
         }
       }
@@ -39,13 +47,12 @@ struct SplashScreenView: View {
         systemImageOpacity = 1.0
         systemImageSize = 150
       }
-
       Task {
         try await Task.sleep(for: .seconds(3))
         withAnimation(.easeIn(duration: 0.35)) {
           zStackOpacity = 0.0
         }
-        try await Task.sleep(for: .seconds(1))
+        try await Task.sleep(for: .seconds(3))
         withAnimation(.easeIn(duration: 0.2)) {
           isPresented.toggle()
         }
